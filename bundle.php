aa1198af62644fbc0535fcb0866419cf1c70e45f
unknown <Michel>
2013-11-03 20:07:20 +1100

    libification

--- bundle.php
+++ bundle.php
@@ -1,4 +1,15 @@
 <?php
-\Coxis\Admin\Libs\AdminMenu::$menu[0]['childs'][] = array('label' => 'Pages', 'link' => 'pages');
+namespace App\Page;
 
-\Coxis\Admin\Libs\AdminMenu::$home[] = array('img'=>\URL::to('page/icon.svg'), 'link'=>'pages', 'title' => 'Pages', 'description' => __('All the static pages.'));
+class Bundle extends \Coxis\Core\BundleLoader {
+	public function load($queue) {
+		parent::load();
+	}
+
+	public function run() {
+		\Coxis\Admin\Libs\AdminMenu::$menu[0]['childs'][] = array('label' => 'Pages', 'link' => 'pages');
+		\Coxis\Admin\Libs\AdminMenu::$home[] = array('img'=>\URL::to('page/icon.svg'), 'link'=>'pages', 'title' => 'Pages', 'description' => __('All the static pages.'));
+		parent::run();
+	}
+}
+return new Bundle;
